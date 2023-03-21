a284e6ae61bd5c2ac680cfcb555a4b104d575910
Kai Eugene <kai.yougene@gmail.com>
2023-03-21 10:08:17 -0700

    creating new object logic, consolidating EL

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import './styles.css';
 
 
 // loading the header and main content container for the page
-import loadPage from './LoadPageHTML.js';
+import loadPage from './PageDom.js';
 loadPage();
 
 import taskItem from './CreateListItem.js';
 taskItem();
 
-const addTaskButton = document.querySelector('.add-task-button');
-const formContainer = document.querySelector('.form-container');
-console.log(formContainer);
-console.log(addTaskButton);
-addTaskButton.addEventListener('click', () => {
-    formContainer.classList.add('show');
-});
+import eventListeners from './EventListeners.js';
+eventListeners();
+
