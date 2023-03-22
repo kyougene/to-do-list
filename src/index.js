8f323e59bb78dac2825990ce932f8eabc14e9413
Kai Eugene <kai.yougene@gmail.com>
2023-03-22 12:25:28 -0700

    functional task creation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import './styles.css';
-
+import { taskArray } from './EventListeners';
 
 // loading the header and main content container for the page
 import loadPage from './PageDom.js';
 loadPage();
 
-import taskItem from './CreateListItem.js';
-taskItem();
-
 import eventListeners from './EventListeners.js';
 eventListeners();
+
 
