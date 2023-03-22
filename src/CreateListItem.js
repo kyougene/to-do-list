16c00fa52cd29be3c0f08044514f30da4f161e05
Kai Eugene <kai.yougene@gmail.com>
2023-03-22 12:25:17 -0700

    creating new task list item

--- src/CreateListItem.js
+++ src/CreateListItem.js
@@ -1,29 +1,42 @@
-export default function (name) {
+import { taskArray } from "./EventListeners.js";
+
+export default function ( taskArray ) {
     const taskUl = document.querySelector('.task-list');
-    //new list item for a new task
-    const newTaskItem = document.createElement('li');
-    newTaskItem.setAttribute('class', 'task-li');
-    taskUl.appendChild(newTaskItem);
-    //complete or incomplete button for each task
-    const taskComplete = document.createElement('svg');
-    taskComplete.setAttribute('class', 'todo-complete');
-    taskComplete.setAttribute('viewBox', '0, 0, 24, 24');
-    taskComplete.innerHTML = '<use xlink:href="./svgs/checkbox-outline.svg#checkbox"></use>';
-    newTaskItem.appendChild(taskComplete);
-    //title of each task
-    const taskTitle = document.createElement('div');
-    taskTitle.setAttribute('class', 'todo-title');
-    taskTitle.innerText = name;
-    newTaskItem.appendChild(taskTitle);
-    //task due date display
-    const taskDueDate = document.createElement('div');
-    taskDueDate.setAttribute('class', 'todo-dueDate');
-    taskDueDate.innerText = 'placeholder';
-    newTaskItem.appendChild(taskDueDate);
-    //delete task  button
-    const taskDelete = document.createElement('svg');
-    taskDelete.setAttribute('class', 'todo-delete');
-    taskDelete.setAttribute('viewBox', '0, 0, 24, 24' );
-    taskDelete.innerHTML = '<use xlink:href="./svgs/trash-can.svg#trashcan"></use>';
-    newTaskItem.appendChild(taskDelete);
+    while (taskUl.firstChild) {
+        taskUl.removeChild(taskUl.firstChild);
+    }
+    if (taskArray.length > 0){
+        taskArray.forEach((object) => {
+            taskUl.innerContent = '';
+            // new list item for a new task
+            const newTaskItem = document.createElement('li');
+            newTaskItem.setAttribute('class', 'task-li');
+            taskUl.appendChild(newTaskItem);
+            //complete or incomplete button for each task
+            const taskComplete = document.createElement('svg');
+            taskComplete.setAttribute('class', 'todo-complete');
+            taskComplete.setAttribute('viewBox', '0, 0, 24, 24');
+            taskComplete.innerHTML = '<use xlink:href="./svgs/checkbox-outline.svg#checkbox"></use>';
+            newTaskItem.appendChild(taskComplete);
+            //title of each task
+            const taskTitle = document.createElement('div');
+            taskTitle.setAttribute('class', 'todo-title');
+            taskTitle.innerText = `${object.taskName}`;
+            newTaskItem.appendChild(taskTitle);
+            //task due date display
+            const taskDueDate = document.createElement('div');
+            taskDueDate.setAttribute('class', 'todo-dueDate');
+            taskDueDate.innerText = `${object.dueDate}`;
+            newTaskItem.appendChild(taskDueDate);
+            //delete task  button
+            const taskDelete = document.createElement('svg');
+            taskDelete.setAttribute('class', 'todo-delete');
+            taskDelete.setAttribute('viewBox', '0, 0, 24, 24' );
+            taskDelete.innerHTML = '<use xlink:href="./svgs/trash-can.svg#trashcan"></use>';
+            newTaskItem.appendChild(taskDelete);
+        })
+    }
+    else {
+        return;
+    }
 }
