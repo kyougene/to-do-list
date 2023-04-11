8fde76e7e240679185cccba5559790918cc5070c
Kai Eugene <kai.yougene@gmail.com>
2023-04-11 09:59:34 -0700

    small changes

--- src/EventListeners.js
+++ src/EventListeners.js
@@ -1,112 +1,125 @@
-import createTaskObject from './TaskObjectFactoryFunction.js';
-import updateTask from './UpdateTask.js';
-import createFile from './FileObjectFactoryFunction.js';
-import updateFile from './updateFile.js'
+import createTaskObject from "./TaskObjectFactoryFunction.js";
+import updateTask from "./UpdateTask.js";
+import createFile from "./FileObjectFactoryFunction.js";
+import updateFile from "./updateFile.js";
 
 const fileArray = [];
 const defaultArray = [];
 
 export default function () {
-    const addTaskButton = document.querySelector('.add-task-button');
-    const formContainer = document.querySelector('.form-container');
-    const form = document.querySelector('.form');
-    const addButton = document.querySelector('#add-button');
-    const taskList = document.querySelector('.task-list');
-    const addFileButton = document.getElementById('add-file-button');
-    const defaultButton = document.getElementById('defaultButton');
-    const fileUl = document.querySelector('.file-list');
-    const taskTitle = document.querySelector('.task-title');
-    let currentlyClicked = defaultArray;
-    
-    
-    //clicking add a task will show the form for a task
-    addTaskButton.addEventListener('click', () => {
-        formContainer.classList.add('show');
+  const addTaskButton = document.querySelector(".add-task-button");
+  const formContainer = document.querySelector(".form-container");
+  const form = document.querySelector(".form");
+  const addButton = document.querySelector("#add-button");
+  const taskList = document.querySelector(".task-list");
+  const addFileButton = document.getElementById("add-file-button");
+  const defaultButton = document.getElementById("defaultButton");
+  const fileUl = document.querySelector(".file-list");
+  const taskTitle = document.querySelector(".task-title");
+  let currentlyClicked = defaultArray;
+
+  //clicking add a task will show the form for a task
+  addTaskButton.addEventListener("click", () => {
+    formContainer.classList.add("show");
+  });
+  //preventing default submit behavior on the form
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+  //creating task object from input values, and updating dom to display task
+  addButton.addEventListener("click", () => {
+    const taskName = form.elements["task-name"].value;
+    const taskDueDate = new Date(form.elements["due-date"].value);
+    const formattedDueDate = taskDueDate.toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
     });
-    //preventing default submit behavior on the form
-    form.addEventListener('submit', (e)=> {
-        e.preventDefault();
-    })
-    //creating task object from input values, and updating dom to display task
-    addButton.addEventListener('click', ()=>{
-        const taskName = form.elements['task-name'].value;
-        const taskDueDate = new Date(form.elements['due-date'].value);
-        const formattedDueDate = taskDueDate.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    if (taskName !== "" || formattedDueDate !== "Invalid Date") {
+      try {
         const task = createTaskObject(taskName, formattedDueDate);
         form.reset();
-        formContainer.classList.remove('show');
+        formContainer.classList.remove("show");
         currentlyClicked.push(task);
         updateTask(currentlyClicked);
-    })
+      } catch {
+        console.log(error);
+      }
+    }
+  });
 
-    //event listener to delete a task
-    taskList.addEventListener('click', (e)=>{
-        if (e.target.classList.contains('todo-delete')){
-            const taskItem = e.target.closest('li');
-            const index = taskItem.dataset.index;
-            currentlyClicked.splice(index, 1);
-            updateTask(currentlyClicked);
-        }
-    })
-    //complete or un-complete button functionality
-    taskList.addEventListener('click', (e)=>{
-        const completeButton = e.target.closest('.todo-complete');
-        if (completeButton) {
-            const taskItem = completeButton.closest('li');
-            const index = taskItem.dataset.index;
-            const task = currentlyClicked[index];
-            task.completed = !(task.completed);
-            if (task.completed){
-                taskItem.classList.add('completed');
-            }
-            else {
-                taskItem.classList.remove('completed');
-            }
-        }
-    })
-    //closing the form when outside the form is clicked
-    document.addEventListener('click', (e)=>{
-        if (!form.contains(e.target) && !formContainer.contains(e.target) && !addTaskButton.contains(e.target)) {
-            form.reset();
-            formContainer.classList.remove('show');
-         }
+  //event listener to delete a task
+  taskList.addEventListener("click", (e) => {
+    if (e.target.classList.contains("todo-delete")) {
+      const taskItem = e.target.closest("li");
+      const index = taskItem.dataset.index;
+      currentlyClicked.splice(index, 1);
+      updateTask(currentlyClicked);
+    }
+  });
+  //complete or un-complete button functionality
+  taskList.addEventListener("click", (e) => {
+    const completeButton = e.target.closest(".todo-complete");
+    if (completeButton) {
+      const taskItem = completeButton.closest("li");
+      const index = taskItem.dataset.index;
+      const task = currentlyClicked[index];
+      task.completed = !task.completed;
+      if (task.completed) {
+        taskItem.classList.add("completed");
+      } else {
+        taskItem.classList.remove("completed");
+      }
+    }
+  });
+  //closing the form when outside the form is clicked
+  document.addEventListener("click", (e) => {
+    if (
+      !form.contains(e.target) &&
+      !formContainer.contains(e.target) &&
+      !addTaskButton.contains(e.target)
+    ) {
+      form.reset();
+      formContainer.classList.remove("show");
+    }
+  });
+
+  addFileButton.addEventListener("click", () => {
+    const div = document.createElement("div");
+    div.setAttribute("class", "add-file-container");
+    div.innerHTML =
+      "<input type='text' name='addFileInput' id='addFileInput' placeholder='Enter File Name'> <button type='submit' id='appendFile'>Add</button> <button id='cancelFile'>Cancel</button>";
+    addFileButton.parentNode.replaceChild(div, addFileButton);
+
+    const cancelFileButton = document.getElementById("cancelFile");
+    cancelFileButton.addEventListener("click", () => {
+      div.parentNode.replaceChild(addFileButton, div);
     });
 
-    addFileButton.addEventListener('click', ()=>{
-        const div = document.createElement('div');
-        div.setAttribute('class', 'add-file-container');
-        div.innerHTML = "<input type='text' name='addFileInput' id='addFileInput' placeholder='Enter File Name'> <button type='submit' id='appendFile'>Add</button> <button id='cancelFile'>Cancel</button>";
-        addFileButton.parentNode.replaceChild(div, addFileButton);
+    const appendFileButton = document.getElementById("appendFile");
+    appendFileButton.addEventListener("click", () => {
+      const fileName = document.getElementById("addFileInput").value;
+      const newFile = createFile(fileName);
+      fileArray.push(newFile);
+      updateFile(fileArray);
+      div.parentNode.replaceChild(addFileButton, div);
+    });
+  });
 
-        const cancelFileButton = document.getElementById('cancelFile');
-        cancelFileButton.addEventListener('click', ()=>{
-            div.parentNode.replaceChild(addFileButton, div);
-        })
+  fileUl.addEventListener("click", (e) => {
+    if (e.target.matches("#file-name-button")) {
+      const index = e.target.dataset.index;
+      currentlyClicked = fileArray[index].tasks;
+      taskTitle.innerHTML = `<h2 class="title">${fileArray[index].name} Tasks</h2>`;
+      updateTask(currentlyClicked);
+    }
+  });
 
-        const appendFileButton = document.getElementById('appendFile');
-        appendFileButton.addEventListener('click', ()=>{
-            const fileName = document.getElementById('addFileInput').value;
-            const newFile = createFile(fileName);
-            fileArray.push(newFile);
-            updateFile(fileArray);
-            div.parentNode.replaceChild(addFileButton, div);
-        })
-        
-    })
+  defaultButton.addEventListener("click", (e) => {
+    currentlyClicked = defaultArray;
+    taskTitle.innerHTML = `<h2 class="title">Default Tasks</h2>`;
 
-    fileUl.addEventListener('click', (e)=>{
-        if (e.target.matches('#file-name-button')) {
-            const index = e.target.dataset.index;
-            currentlyClicked = fileArray[index].tasks;
-            taskTitle.innerHTML = `<h2 class="title">${fileArray[index].name} Tasks</h2>`
-            updateTask(fileArray[index].tasks);
-        }
-    })
-
-    defaultButton.addEventListener('click', (e)=>{
-        currentlyClicked = defaultArray;
-        taskTitle.innerHTML = `<h2 class="title">Default Tasks</h2>`
-
-        updateTask(currentlyClicked);
-    })
+    updateTask(currentlyClicked);
+  });
 }
--- src/PageDom.js
+++ src/PageDom.js
@@ -1,101 +1,99 @@
 export default function () {
-    //creating the header and main body
-    const container = document.querySelector('.container')
-    const header = document.createElement('header');
-    header.setAttribute('class', 'header');
-    header.innerHTML = '<h1>To Do List</h1>';
-    container.appendChild(header);
-    const mainContent = document.createElement('div');
-    mainContent.setAttribute('class', 'main-content');
-    container.appendChild(mainContent);
+  //creating the header and main body
+  const container = document.querySelector(".container");
+  const header = document.createElement("header");
+  header.setAttribute("class", "header");
+  header.innerHTML = "<h1>To Do List</h1>";
+  container.appendChild(header);
+  const mainContent = document.createElement("div");
+  mainContent.setAttribute("class", "main-content");
+  container.appendChild(mainContent);
 
+  //creating the file bar
+  const fileBar = document.createElement("div");
+  fileBar.setAttribute("class", "file-bar");
+  mainContent.appendChild(fileBar);
+  //container for buttons
+  const buttonContainer = document.createElement("div");
+  buttonContainer.setAttribute("class", "button-container");
+  fileBar.appendChild(buttonContainer);
+  //creating the add a file button, as a list item under the file list
+  const addAFileButton = document.createElement("button");
+  addAFileButton.setAttribute("class", "button-list");
+  addAFileButton.setAttribute("id", "add-file-button");
+  addAFileButton.textContent = "+ Add A File";
+  buttonContainer.appendChild(addAFileButton);
+  //creating the default file, also as a listem item under the file list
+  const defaultButton = document.createElement("button");
+  defaultButton.setAttribute("class", "button-list");
+  defaultButton.setAttribute("id", "defaultButton");
+  defaultButton.textContent = "Default";
+  buttonContainer.appendChild(defaultButton);
+  //creating list of files
+  const fileList = document.createElement("ul");
+  fileList.setAttribute("class", "file-list");
+  fileBar.appendChild(fileList);
 
-    //creating the file bar
-    const fileBar = document.createElement('div');
-    fileBar.setAttribute('class', 'file-bar');
-    mainContent.appendChild(fileBar);
-    //container for buttons 
-    const buttonContainer = document.createElement('div');
-    buttonContainer.setAttribute('class', 'button-container');
-    fileBar.appendChild(buttonContainer)
-    //creating the add a file button, as a list item under the file list
-    const addAFileButton = document.createElement('button');
-    addAFileButton.setAttribute('class', 'button-list');
-    addAFileButton.setAttribute('id', 'add-file-button');
-    addAFileButton.textContent = '+ Add A File';
-    buttonContainer.appendChild(addAFileButton);
-    //creating the default file, also as a listem item under the file list
-    const defaultButton = document.createElement('button');
-    defaultButton.setAttribute('class', 'button-list');
-    defaultButton.setAttribute('id', 'defaultButton');
-    defaultButton.textContent = 'Default';
-    buttonContainer.appendChild(defaultButton);
-    //creating list of files
-    const fileList = document.createElement('ul');
-    fileList.setAttribute('class', 'file-list');
-    fileBar.appendChild(fileList);
-    
-    
-    //creating the task container for task content
-    const taskContainer = document.createElement('div');
-    taskContainer.setAttribute('class', 'task-container');
-    mainContent.appendChild(taskContainer);
-    //creating the title for task container
-    const taskContainerTitle = document.createElement('div');
-    taskContainerTitle.setAttribute('class', 'task-title');
-    taskContainerTitle.innerHTML = '<h2 class="title">Default Tasks</h2>';
-    taskContainer.appendChild(taskContainerTitle);
-    //add task button
-    const addTask = document.createElement('button');
-    addTask.setAttribute('class', 'add-task-button');
-    addTask.innerText = '+ Add A Task';
-    taskContainer.appendChild(addTask);
-    //task list ul
-    const taskList = document.createElement('ul');
-    taskList.setAttribute('class', 'task-list');
-    taskContainer.appendChild(taskList);
+  //creating the task container for task content
+  const taskContainer = document.createElement("div");
+  taskContainer.setAttribute("class", "task-container");
+  mainContent.appendChild(taskContainer);
+  //creating the title for task container
+  const taskContainerTitle = document.createElement("div");
+  taskContainerTitle.setAttribute("class", "task-title");
+  taskContainerTitle.innerHTML = '<h2 class="title">Default Tasks</h2>';
+  taskContainer.appendChild(taskContainerTitle);
+  //add task button
+  const addTask = document.createElement("button");
+  addTask.setAttribute("class", "add-task-button");
+  addTask.innerText = "+ Add A Task";
+  taskContainer.appendChild(addTask);
+  //task list ul
+  const taskList = document.createElement("ul");
+  taskList.setAttribute("class", "task-list");
+  taskContainer.appendChild(taskList);
 
-    
-    //creating the add a task form
-    const formContainer = document.createElement('div');
-    formContainer.setAttribute('class', 'form-container');
-    container.appendChild(formContainer);
-    //creating the form title
-    const formTitle = document.createElement('h2');
-    formTitle.setAttribute('id', 'form-title');
-    formTitle.innerText = 'Add A Task';
-    formContainer.appendChild(formTitle);
-    //creating the form element
-    const form = document.createElement('form');
-    form.setAttribute('class', 'form');
-    formContainer.appendChild(form);
-    //creating the label for the task name input field
-    const taskLabel = document.createElement('label');
-    taskLabel.setAttribute('for', 'task-name');
-    taskLabel.innerText = 'Task Name:';
-    form.appendChild(taskLabel);
-    //creating the task name input field
-    const taskName = document.createElement('input');
-    taskName.setAttribute('type', 'text');
-    taskName.setAttribute('id', 'task-name');
-    taskName.setAttribute('name', 'task-name')
-    taskName.setAttribute('placeholder', 'Enter Task Name');
-    form.appendChild(taskName);
-    //label for the due date field
-    const dueDateLabel = document.createElement('label');
-    dueDateLabel.setAttribute('for', 'due-date');
-    dueDateLabel.innerText = 'Due Date:';
-    form.appendChild(dueDateLabel);
-    //creating the due date input field
-    const dueDate = document.createElement('input');
-    dueDate.setAttribute('type', 'date');
-    dueDate.setAttribute('id', 'due-date');
-    dueDate.setAttribute('name', 'due-date');
-    form.appendChild(dueDate);
-    //button to add a task after form is created
-    const addButton = document.createElement('button');
-    addButton.setAttribute('type', 'submit');
-    addButton.setAttribute('id', 'add-button');
-    addButton.innerText = 'Add Task';
-    form.appendChild(addButton);
+  //creating the add a task form
+  const formContainer = document.createElement("div");
+  formContainer.setAttribute("class", "form-container");
+  container.appendChild(formContainer);
+  //creating the form title
+  const formTitle = document.createElement("h2");
+  formTitle.setAttribute("id", "form-title");
+  formTitle.innerText = "Add A Task";
+  formContainer.appendChild(formTitle);
+  //creating the form element
+  const form = document.createElement("form");
+  form.setAttribute("class", "form");
+  formContainer.appendChild(form);
+  //creating the label for the task name input field
+  const taskLabel = document.createElement("label");
+  taskLabel.setAttribute("for", "task-name");
+  taskLabel.innerText = "Task Name:";
+  form.appendChild(taskLabel);
+  //creating the task name input field
+  const taskName = document.createElement("input");
+  taskName.setAttribute("type", "text");
+  taskName.setAttribute("id", "task-name");
+  taskName.setAttribute("name", "task-name");
+  taskName.setAttribute("placeholder", "Enter Task Name");
+  taskName.required = true;
+  form.appendChild(taskName);
+  //label for the due date field
+  const dueDateLabel = document.createElement("label");
+  dueDateLabel.setAttribute("for", "due-date");
+  dueDateLabel.innerText = "Due Date:";
+  form.appendChild(dueDateLabel);
+  //creating the due date input field
+  const dueDate = document.createElement("input");
+  dueDate.setAttribute("type", "date");
+  dueDate.setAttribute("id", "due-date");
+  dueDate.setAttribute("name", "due-date");
+  form.appendChild(dueDate);
+  //button to add a task after form is created
+  const addButton = document.createElement("button");
+  addButton.setAttribute("type", "submit");
+  addButton.setAttribute("id", "add-button");
+  addButton.innerText = "Add Task";
+  form.appendChild(addButton);
 }
